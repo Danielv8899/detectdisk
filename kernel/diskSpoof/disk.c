ceeb6062e5b3e0bffbeff2d511a82977267b63fe
Danielv8899 <danielv8899@gmail.com>
2022-10-26 14:55:05 +0300

    added support for SMART_RCV_DRIVE_DATA

--- kernel/diskSpoof/disk.c
+++ kernel/diskSpoof/disk.c
@@ -1,5 +1,8 @@
 #include "disk.h"
 
+#define QWORD ULONGLONG
+#define SMART_PARAMS_SIZE sizeof(SENDCMDOUTPARAMS) + IDENTIFY_BUFFER_SIZE - 1
+
 VOID ChangeIoc(PIO_STACK_LOCATION ioc, PIRP irp, PIO_COMPLETION_ROUTINE routine) {
 	PIOC_REQUEST request = (PIOC_REQUEST)ExAllocatePool2(NonPagedPool, sizeof(IOC_REQUEST), 0x6e556353);
 	if (!request) {
@@ -41,14 +44,42 @@ NTSTATUS StorageQueryIoc(PDEVICE_OBJECT device, PIRP irp, PVOID context) {
 	return STATUS_SUCCESS;
 }
 
+NTSTATUS SmartRcvIoc(PDEVICE_OBJECT device, PIRP irp, PVOID context) {
+	if (context) {
+		IOC_REQUEST request = *(PIOC_REQUEST)context;
+		ExFreePool(context);
+
+		if (request.BufferLength >= sizeof(PSENDCMDINPARAMS)) {
+			PSENDCMDINPARAMS desc = (PSENDCMDINPARAMS)request.Buffer;
+			QWORD offset = (QWORD)desc->bBuffer - (QWORD)desc;
+			if (offset && offset < request.BufferLength) {
+				strcpy((PCHAR)desc + offset, NAME);
+			}
+		}
+		if (request.OldRoutine && irp->StackCount > 1) {
+			return request.OldRoutine(device, irp, request.OldContext);
+		}
+	}
+	return STATUS_SUCCESS;
+}
+
 NTSTATUS DiskControl(PDEVICE_OBJECT device, PIRP irp) {
 	PIO_STACK_LOCATION ioc = IoGetCurrentIrpStackLocation(irp);
+	PVOID buffer = irp->AssociatedIrp.SystemBuffer;
 	switch (ioc->Parameters.DeviceIoControl.IoControlCode) {
 	case IOCTL_STORAGE_QUERY_PROPERTY:
-		if (StorageDeviceProperty == ((PSTORAGE_PROPERTY_QUERY)irp->AssociatedIrp.SystemBuffer)->PropertyId) {
+		if (StorageDeviceProperty == ((PSTORAGE_PROPERTY_QUERY)buffer)->PropertyId) {
 			ChangeIoc(ioc, irp, StorageQueryIoc);
 		}
 		break;
+	
+	case SMART_RCV_DRIVE_DATA:
+	{
+		PSENDCMDINPARAMS pCmd = (PSENDCMDINPARAMS)buffer;
+		if (pCmd->irDriveRegs.bCommandReg == ID_CMD) {
+			ChangeIoc(ioc, irp, SmartRcvIoc);
+		}
+	}
 	}
 	return DiskControlOriginal(device, irp);
 }
--- user/detectdisk/detectdisk.cpp
+++ user/detectdisk/detectdisk.cpp
@@ -5,6 +5,35 @@
 #include <winioctl.h>
 #include <list>
 
+#define  DFP_GET_VERSION          0x00074080
+#define  IDE_ATAPI_IDENTIFY  0xA1  
+#define  IDE_ATA_IDENTIFY    0xEC 
+#define  DFP_RECEIVE_DRIVE_DATA   0x0007c088
+
+typedef struct _GETVERSIONOUTPARAMS {
+    BYTE bVersion;     
+    BYTE bRevision;    
+    BYTE bReserved;     
+    BYTE bIDEDeviceMap; 
+    DWORD fCapabilities; 
+    DWORD dwReserved[4]; 
+} GETVERSIONOUTPARAMS, * PGETVERSIONOUTPARAMS, * LPGETVERSIONOUTPARAMS;
+
+bool drv_convert_to_string(DWORD diskdata[256], DWORD diskdata_size, unsigned int firstIndex, unsigned int lastIndex, std::string& buffer) {
+    unsigned int index = 0;
+
+    if (firstIndex > lastIndex || firstIndex >= diskdata_size || lastIndex >= diskdata_size)
+        return false;
+
+    for (index = firstIndex; index <= lastIndex; index++) {
+        buffer += static_cast<char>((diskdata[index] / 256));
+        buffer += static_cast<char>((diskdata[index] % 256));
+    }
+
+    return true;
+}
+
+
 int main()
 {
     STORAGE_PROPERTY_QUERY pQuery;
@@ -15,24 +44,23 @@ int main()
     BYTE* pOutBuffer = NULL;
     std::list<std::string> drvModelName;
     char a;
-    DWORD lastErr = GetLastError();
 
     std::wstring device = L"\\\\.\\PhysicalDrive0";
     ZeroMemory(&pQuery, sizeof(STORAGE_PROPERTY_QUERY));
     pQuery.PropertyId = StorageDeviceProperty;
     pQuery.QueryType = PropertyStandardQuery;
 
-    hDevice = CreateFileW(device.c_str() , 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+    hDevice = CreateFileW(device.c_str() , GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
     if (hDevice == INVALID_HANDLE_VALUE) {
-        std::cout << lastErr;
+        std::cout << GetLastError() << std::endl;
         std::cin >> a;
         return -1;
     }
 
     bResult = DeviceIoControl(hDevice, IOCTL_STORAGE_QUERY_PROPERTY, &pQuery, sizeof(STORAGE_PROPERTY_QUERY), &pHdr, sizeof(STORAGE_DESCRIPTOR_HEADER), &dwBytesReturned, NULL);
         if (!bResult){
-            std::cout << lastErr;
+            std::cout << GetLastError() << std::endl;;
             std::cin >> a;
             return -1;
         }
@@ -42,7 +70,7 @@ int main()
     ZeroMemory(pOutBuffer, dwOutBufferSize);
 
     if(!DeviceIoControl(hDevice, IOCTL_STORAGE_QUERY_PROPERTY, &pQuery, sizeof(STORAGE_PROPERTY_QUERY), pOutBuffer, dwOutBufferSize, &dwBytesReturned, NULL)){
-            std::cout << lastErr;
+            std::cout << GetLastError() << std::endl;;
             std::cin >> a;
             return -1;
         }
@@ -65,6 +93,73 @@ int main()
     auto st = drvModelName.begin();
     std::cout << "prod id: " << *st << std::endl;
     std::cin >> a;
+
+    GETVERSIONOUTPARAMS VersionParams;
+    DWORD cbBytesReturned = 0;
+    BYTE IdOutCmd[sizeof(SENDCMDOUTPARAMS) + IDENTIFY_BUFFER_SIZE - 1];
+
+    memset((void*)&VersionParams, 0, sizeof(VersionParams));
+
+    if (!DeviceIoControl(hDevice, DFP_GET_VERSION, NULL, 0, &VersionParams, sizeof(VersionParams), &cbBytesReturned, NULL)) {
+        CloseHandle(hDevice);
+        std::cout << "failed DFP_GET_VERSION" << std::endl;
+        std::cin >> a;
+        return -1;
+    }
+
+    BYTE bIDCmd = 0;
+    SENDCMDINPARAMS scip;
+
+    bIDCmd = (VersionParams.bIDEDeviceMap >> 0 & 0x10) ? IDE_ATAPI_IDENTIFY : IDE_ATA_IDENTIFY;
+
+    memset(&scip, 0, sizeof(scip));
+    memset(IdOutCmd, 0, sizeof(IdOutCmd));
+
+    PSENDCMDINPARAMS pSCIP = &scip;
+    PSENDCMDOUTPARAMS pSCOP = reinterpret_cast<PSENDCMDOUTPARAMS>(&IdOutCmd);
+    BYTE bbIDCmd = static_cast<BYTE>(bIDCmd);
+    PDWORD lpcbBytesReturned = 0;
+
+
+    pSCIP->cBufferSize = IDENTIFY_BUFFER_SIZE;
+    pSCIP->irDriveRegs.bFeaturesReg = 0;
+    pSCIP->irDriveRegs.bSectorCountReg = 1;
+    pSCIP->irDriveRegs.bCylLowReg = 0;
+    pSCIP->irDriveRegs.bCylHighReg = 0;
+
+    pSCIP->irDriveRegs.bDriveHeadReg = 0xA0 | ((0 & 1) << 4);
+
+    pSCIP->irDriveRegs.bCommandReg = bbIDCmd;
+    pSCIP->bDriveNumber = 0;
+    pSCIP->cBufferSize = IDENTIFY_BUFFER_SIZE;
+
+    if (!(DeviceIoControl(
+        hDevice,
+        DFP_RECEIVE_DRIVE_DATA,
+        static_cast<LPVOID>(pSCIP),
+        sizeof(SENDCMDINPARAMS) - 1,
+        static_cast<LPVOID>(pSCOP),
+        sizeof(SENDCMDOUTPARAMS) + IDENTIFY_BUFFER_SIZE - 1,
+        lpcbBytesReturned, NULL))) {
+
+        CloseHandle(hDevice);
+        std::cout << "failed DFP_RECEIVE_DRIVE_DATA" << std::endl;
+        std::cin >> a;
+        return -1;
+    }
+
+    DWORD diskdata[256];
+    USHORT* pIdSector = reinterpret_cast<USHORT*>((reinterpret_cast<PSENDCMDOUTPARAMS>(IdOutCmd))->bBuffer);
+    std::string drive_model;
+
+    for (int i = 0; i < 256; i++) {
+        diskdata[i] = pIdSector[i];
+    }
+
+    drv_convert_to_string(diskdata, _countof(diskdata), 27, 46, drive_model);
+    std::cout << "drive_model: " << drive_model.c_str() << std::endl;
+    std::cin >> a;
+
     CloseHandle(hDevice);
     if (pOutBuffer) {
         delete[] pOutBuffer;
